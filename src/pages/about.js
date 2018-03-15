98316a1ea215c5e69a884521ab84bc67468e189b
Darren Cox <Darren_Cox@discovery.com>
2018-03-15 12:55:52 +0000

    windows

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,28 +3,20 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import styles from './blog.module.css'
-import ArticlePreview from '../components/article-preview'
+
 
 class aboutPage extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+
 
     return (
       <div style={{ background: '#fff' }}>
         <Helmet title={siteTitle} />
         <div className="wrapper">
-          <div className={styles.hero}>Blog</div>
-          <h2 className="section-headline">Recent articles</h2>
-          <ul className="article-list">
-            {posts.map(({ node }) => {
-              return (
-                <li key={node.slug}>
-                  <ArticlePreview article={node} />
-                </li>
-              )
-            })}
-          </ul>
+          <div className={styles.hero}>About</div>
+          <h2 className="section-headline">About</h2>
+          <p>Hey</p>
         </div>
       </div>
     )
@@ -32,28 +24,3 @@ class aboutPage extends React.Component {
 }
 
 export default aboutPage
-
-export const pageQuery = graphql`
-  query BlogIndexQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      edges {
-        node {
-          title
-          slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          heroImage {
-            file {
-              url
-            }
-          }
-          description {
-            childMarkdownRemark {
-              html
-            }
-          }
-        }
-      }
-    }
-  }
-`
