89961dc3b70b05eb5e23294f780a533e0d9a65f5
Darren Cox <Darren_Cox@discovery.com>
2018-03-15 12:30:51 +0000

    windows

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+import styles from './blog.module.css'
+import ArticlePreview from '../components/article-preview'
 
 class AboutPage extends Component {
   render() {
     return (
-      <div className="about-container">
+      <div className="wrapper">
         <p>About me.</p>
       </div>
     );
