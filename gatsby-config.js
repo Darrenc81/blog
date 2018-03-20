042a82df54b1961ee965cf8b561d96c3b6bf15ae
Darren Cox <darrenlcox@icloud.com>
2018-03-20 09:50:15 +0000

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,15 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID || '',
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+      
+        trackingId: "UA-116081281-1",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      
       }
     }
   ]
