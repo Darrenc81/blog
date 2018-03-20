badb04fe863ad2703305fad96f34a0e053193b7b
Darren Cox <darrenlcox@icloud.com>
2018-03-20 10:02:53 +0000

    google fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,18 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID || '',
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
       
-        trackingId: "UA-116081281-1",
+       
+      
+      },
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+       trackingId: "UA-116081281-1",
         // Puts tracking script in the head instead of the body
         head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,
-      
       }
     }
   ]
